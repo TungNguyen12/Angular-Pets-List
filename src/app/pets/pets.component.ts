fb5f4a8a9b1ebc0aa1113840ced40fd5ce2a60ff
TungNguyen12 <nvtung382@gmail.com>
2024-02-21 23:02:52 +0200

    adding seprte component pet-detail out of pet component for maintainability

--- src/app/pets/pets.component.ts
+++ src/app/pets/pets.component.ts
@@ -3,13 +3,21 @@ import { Pet } from '../pet'
 import { CommonModule, NgFor, NgIf, UpperCasePipe } from '@angular/common'
 import { FormsModule } from '@angular/forms'
 import { PETS } from '../pet-list'
+import { PetInfoComponent } from '../pet-info/pet-info.component'
 
 @Component({
   selector: 'app-pets',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgFor, NgIf, UpperCasePipe],
   templateUrl: './pets.component.html',
   styleUrl: './pets.component.css',
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgFor,
+    NgIf,
+    UpperCasePipe,
+    PetInfoComponent,
+  ],
 })
 export class PetsComponent {
   pets = PETS
